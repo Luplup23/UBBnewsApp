cba157d438bd447a7c8d5133a2ac64e73efc08e6
Luplup23 <122296083+Luplup23@users.noreply.github.com>
2024-08-16 17:59:08 +0200

    Wrong files part 2

--- UBBnewsApp/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/UBBnewsApp.AssemblyInfo.cs
+++ UBBnewsApp/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/UBBnewsApp.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UBBnewsApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+62a6cc4fce5d66f515efc4e898a970953f446400")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+2a88d11ff4cf3fc4032c6d27b4752793d4ecd948")]
 [assembly: System.Reflection.AssemblyProductAttribute("UBBnewsApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UBBnewsApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
